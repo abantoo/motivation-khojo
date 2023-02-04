15a15391df72f2916f9c123a7bf92d042df4bbbb
Abanto Naimur <abantonaimur@gmail.com>
2023-02-05 03:21:32 +0600

    First Commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <h1 className='text-red-500'>Welcome to Motivation Khojo</h1>
+    </div>
+  )
+}
+
+export default App
