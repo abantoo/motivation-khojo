4a48197737878b755111f382c33a57a172d73cc4
Abanto Naimur <abantonaimur@gmail.com>
2023-02-06 02:28:49 +0600

    adds some basic style

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,8 +1,17 @@
 function Nav() {
     return(
-        <div>
-            <h1>Motivation khojo</h1>
-            <button>Click Me</button>
+        <div className="flex items-center justify-between bg-blue-300 py-4 px-2">
+            <div className="flex">
+                <h1>Motivation</h1>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+                </svg>
+            </div>
+            <button>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 13.5l10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75z" />
+                </svg>
+            </button>
         </div>
     )
 }
