d37f698e41ea222499e67ab0bedc1dc7f43dbb73
Abanto Naimur <abantonaimur@gmail.com>
2023-02-06 02:20:21 +0600

    adds router

---
+++ src/components/Nav.tsx
@@ -0,0 +1,10 @@
+function Nav() {
+    return(
+        <div>
+            <h1>Motivation khojo</h1>
+            <button>Click Me</button>
+        </div>
+    )
+}
+
+export default Nav;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,9 @@
+function Home() {
+    return(
+        <div>
+            <h1>Welcome to Motivation khojo</h1>
+        </div>
+    )
+}
+
+export default Home;
