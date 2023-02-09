555f9d69e937a4e914923bd1575420f1ae9b03ec
Abanto Naimur <abantonaimur@gmail.com>
2023-02-09 19:43:47 +0600

    new: layout for feed

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,25 @@ import {
   RouterProvider,
   createBrowserRouter,
   createRoutesFromElements,
-  Route
-} from 'react-router-dom'
-import RootLayout from './pages/RootLaytout';
-import HomePage from './pages/Home'
-import AboutPage from './pages/About'
+  Route,
+} from "react-router-dom";
+import RootLayout from "./pages/RootLaytout";
+import HomePage from "./pages/Home";
+import AboutPage from "./pages/About";
+import FeedPage from "./pages/Feed";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path='/' element={<RootLayout/>}>
-      <Route index element={<HomePage/>}/>
-      <Route path='about' element={<AboutPage/>}/>
+    <Route path="/" element={<RootLayout />}>
+      <Route index element={<HomePage />} />
+      <Route path="about" element={<AboutPage />} />
+      <Route path="feed" element={<FeedPage />} />
     </Route>
   )
 );
 
 function App() {
-  return (
-    <RouterProvider router={router} />
-  )
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
