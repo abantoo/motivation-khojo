5ecdfc538b5c7717d8c812099806701ee70444ab
abantoo <abantonaimur@gmail.com>
2023-07-04 23:00:40 +0600

    adds dummy page

---
+++ src/pages/Users.tsx
@@ -0,0 +1,26 @@
+import KhojoBird from "../assets/KhojoBird.svg";
+function Feed() {
+    return (
+        <div className="flex justify-center md:w-1/2 bg-slate-200 mx-auto">
+            <div className="md:w-1/2 w-full m-2 bg-slate-600 h-max p-4 rounded shadow shadow-slate-300">
+                <div className="rounded-full border-2 border-blue-400 h-10 w-10 flex items-center justify-end bg-red-300">
+                    <img
+                        src={KhojoBird}
+                        alt="Khojo, kisu paile boilo"
+                        className="h-8 w-8"
+                    />
+                </div>
+                <div>
+                    <h2 className="font-bold">I Lost my motivation</h2>
+                    <p>
+                        After sharing my generation breaking startup idea with a guy on
+                        discord, he stole the idea and trademarked it. I lost my motivation,
+                        but gained it back after visiting motivation khojo
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Feed;
